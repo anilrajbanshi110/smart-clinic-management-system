96ed20af9d04b9dbc047afa771d8872d2376fd01
Anil Rajbanshi <anilrajbanshi145gmail.com@Anils-MacBook-Air.local>
2026-05-24 23:52:40 -0400

    Added Doctor.java and schema design

---
+++ src/main/java/com/example/smartclinic/model/Doctor.java
@@ -0,0 +1,59 @@
+package com.example.smartclinic.model;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long doctorId;
+
+    private String name;
+
+    private String specialization;
+
+    @ElementCollection
+    private List<String> availableTimes;
+
+    public Doctor() {
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public List<String> getAvailableTimes() {
+        return availableTimes;
+    }
+
+    public void setAvailableTimes(List<String> availableTimes) {
+        this.availableTimes = availableTimes;
+    }
+}
